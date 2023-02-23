86bf5cd971ff2b226d245411e4c7d905a8dd965b
karthik1131 <rangana.dinesh09@gmail.com>
2023-02-23 21:17:38 +0530

    firstcommit

---
+++ src/components/Newpage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class Newpage extends Component {
+  
+
+  render() {
+    return <div id="display-form-data">{}</div>;
+  }
+}
+
+export default Newpage;
